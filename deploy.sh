1cf760283a759b3e350c043012ba107409a70047
mamathapoluru <81650834+mamathapoluru@users.noreply.github.com>
2021-04-01 00:09:01 -0400

    Create deploy.sh
    added deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "$KUBERNETES_CLUSTER_CERTIFICATE" | base64 --decode > cert.crt
+/usr/local/bin/kubectl \
+  --kubeconfig=/dev/null \
+  --server=$KUBERNETES_SERVER \
+  --certificate-authority=cert.crt \
+  --token=$KUBERNETES_TOKEN \
+  apply -f ./kube/
